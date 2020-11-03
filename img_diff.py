c8e6e91548f6d6f60f2784022f854f93b56384a7
Maxim Obrubov <m.obrubov@yandex.ru>
2020-11-03 17:31:22 +0400

    img diff script

---
+++ img_diff.py
@@ -0,0 +1,37 @@
+from PIL import Image, ImageOps, ImageChops
+import numpy
+
+first_img_path = "samples/320_etalon.png"
+second_img_path = "samples/320_.png"
+
+with Image.open(first_img_path).convert('LA') as first:
+    with Image.open(second_img_path).convert('LA') as second:
+        if first.size[1] > second.size[1]:
+            second = second.resize(first.size)
+        else:
+            first = first.resize(second.size)
+        
+        first = first.convert('RGBA')
+        # NOTE: inverted image over initial should give monochromic color
+        second = ImageOps.invert(second.convert('RGB')).convert('RGBA')
+        
+        first.putalpha(128)
+        second.putalpha(128)
+        first.paste(second, None, second)
+        first = first.convert('RGB')
+        first.save("overlay.png")
+        
+        np_im = numpy.array(first)
+        width, height, colors_count = np_im.shape
+        difference = 0
+        
+        for x in range(width):
+            for y in range(height):
+                if not numpy.array_equal([127, 127, 127], np_im[x][y]):
+                    difference += 1
+                    np_im[x][y] = [127, 0, 0]
+        
+        print("{:.2%}".format(difference / (width * height)))
+        new_im = Image.fromarray(np_im)
+        new_im.save("result.png")
+        
